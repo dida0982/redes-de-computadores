d9cc06bb0ceff94d053bf385274e1638eba4d651
dida0982 <dida0982@gmail.com>
2026-03-20 22:29:24 +0000

    20 de mar. de 2026, 19:29

--- cap2ex2.py
+++ cap2ex2.py
@@ -1,1 +1,3 @@
-#Como parte da Internet das Coisas, os dispositivos do dia a dia estão cade vez mais conectados a redes de computador. A IoT facilita as pessoas, entre outras coisas, monitorar seus pertences e o uso aparelhos. Mas qualqer tecnologia pode ser usada tanto para o bem quanto para o mal. Discuta algumas desvantagens dessa tecnologia. 
+#Como parte da Internet das Coisas, os dispositivos do dia a dia estão cade vez mais conectados a redes de computador. A IoT(internet das coisas) facilita as pessoas, entre outras coisas, monitorar seus pertences e o uso aparelhos. Mas qualqer tecnologia pode ser usada tanto para o bem quanto para o mal. Discuta algumas desvantagens dessa tecnologia.
+
+#Infelizmente criminosos podem usar de aparelhos ligados a internet para cometer diversos crimes. Como clonagem de cartão, obtenção de senhas, vazamento de dados, 
