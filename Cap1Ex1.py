6aa23aff405f075b8c469f9d5f9ee5a3e9b3e69a
dida0982 <dida0982@gmail.com>
2026-03-20 21:54:29 +0000

    20 de mar. de 2026, 18:54

--- Cap1Ex1.py
+++ Cap1Ex1.py
@@ -2,25 +2,28 @@
 
 #Exercício: Você estabelece um canal de cominucação entre dois castelos medievais, permitindo que um corvo treinado carregue rapidamente um pergaminho do castelo que enviou ao castelo que recebe, a 160km de distância. o corvo voa a uma velocidade média de 40 km/h e carrega um pergaminho de cada vez. Cada pergaminho contem 1,8 tb de dados. Calcule a taxa de dados deste canal ao enviar (i) 1,8 tb de dados; (ii) 3,6 tb de dados; (iii) fluxo infinito de dados.
 
-#Dados do Problema:
-distancia = 160  # 160km
-velocidade = 40  # 40km/h
-tempo = distancia/velocidade # 4h
-capacidade_pergaminho = 1.8 # 1,8TB
-#um pergaminho por viagem
-taxa = capacidade_pergaminho/tempo #0.45 TB/h
+distancia = 160  # km
+velocidade = 40  # km/h
+tempo = distancia / velocidade  # 4h
+capacidade_pergaminho = 1.8     # TB
 
+# taxa em TB/h
+taxa_TB_h = capacidade_pergaminho / tempo
+
+# conversões
+bytes_por_TB = 10**12
+bits_por_byte = 8
+segundos_por_hora = 3600
+
+# taxa em bits/s
+taxa_bps = (taxa_TB_h * bytes_por_TB * bits_por_byte) / segundos_por_hora
 
 print('(i) Envio de 1,8 TB')
-print('Taxa de dados: ', taxa, 'TB/h' )
-converter_bps = taxa * (10**12) * 8 /3600
-print(f'Convertido para bps: {converter_bps:,.2f} (1 Gbps)')
+print(f'Taxa de dados: {taxa_TB_h} TB/h')
+print(f'Convertido para bps: {taxa_bps:,.2f} (~1 Gbps)\n')
 
-print("")
-print('(ii) Envio de 3.6 tb de dados:')
-taxa2 = (capacidade_pergaminho*2)/(tempo*2)
-print('Taxa de dados: ', taxa2 )
+print('(ii) Envio de 3,6 TB')
+print(f'Taxa de dados: {taxa_TB_h} TB/h (mesma taxa)\n')
 
-print("")
-print('(iii) Fluxo infinito de dados (O corvo faz viagens contínuas, sempre levando 1,8 TB a cada 4 horas.):')
-print('Taxa de dados: ', taxa, 'TB/h' )
+print('(iii) Fluxo infinito de dados')
+print(f'Taxa de dados sustentada: {taxa_TB_h} TB/h (~1 Gbps)')
