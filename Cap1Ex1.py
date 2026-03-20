443fad2b8641a20f7af1c98b6aa3195d37e5b059
dida0982 <dida0982@gmail.com>
2026-03-20 18:34:02 -0300

    ...
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- Cap1Ex1.py
+++ Cap1Ex1.py
@@ -1,26 +1,20 @@
 # Capítulo 1 - Exercício 1
-#Throughput: Taxa de dados, ou throughput, é a quantidade de dados que pode ser transmitida por unidade de tempo. A taxa de dados é geralmente expressa em bits por segundo (bps), mas também pode ser expressa em bytes por segundo (Bps), kilobits por segundo (kbps), megabits por segundo (Mbps), gigabits por segundo (Gbps), etc.
 
 #Exercício: Você estabelece um canal de cominucação entre dois castelos medievais, permitindo que um corvo treinado carregue rapidamente um pergaminho do castelo que enviou ao castelo que recebe, a 160km de distância. o corvo voa a uma velocidade média de 40 km/h e carrega um pergaminho de cada vez. Cada pergaminho contem 1,8 tb de dados. Calcule a taxa de dados deste canal ao enviar (i) 1,8 tb de dados; (ii) 3,6 tb de dados; (iii) fluxo infinito de dados.
 
 #Dados do Problema:
-distancia = 160  # km
-velocidade = 40  # km/h
-dados_por_pergaminho = 1.8  # TB
-tempo_de_viagem = distancia / velocidade  # 160 / 40 = 4 horas. Em 4h ele faz todo o percurso.
+distancia = 160  # 160km
+velocidade = 40  # 40km/h
+tempo = distancia/velocidade # 4h
+capacidade_pergaminho = 1.8 # 1,8TB
+#um pergaminho por viagem
+taxa = capacidade_pergaminho/tempo #0.45 TB/h
 
-#taxa de dados:
-taxa = dados_por_pergaminho / tempo_de_viagem  # 1.8 TB / 4h = 0.45 TB/h
-print('Distancia: 160 km')
-print('Velocidade: 40 km/h')
-print(f'Tempo de viagem: {tempo_de_viagem} h')
-print("Dados por pergaminho: 1.8 TB")
+
+print('(i) Envio de 1,8 TB')
+print('Taxa de dados: ', taxa, 'TB/h' )
+converter_bps = taxa * (10**12) * 8 /3600
+print(f'Convertido para bps: {converter_bps:,.2f} (1 Gbps)')
 
 print("")
-
-print(f'(i) Taxa de dados para 1,8 TB: {taxa} TB/h')
-print("")
-print(f'(ii) Taxa de dados para 3,6 TB: {taxa} TB/h')
-print("")
-print(f'(iii) Taxa de dados para fluxo infinito de dados: {taxa} TB/h')
-
+print('(ii) 3.6 tb de dados:')
