229bef5354925d7462f0272818b577a812e01f76
dida0982 <dida0982@gmail.com>
2026-04-04 15:44:28 +0000

    4 de abr. de 2026, 12:44

--- cap1ex10.py
+++ cap1ex10.py
@@ -1,3 +1,5 @@
 # Um sinal viaja a 2/3 da velocidade da luz e leva 100 milissegundos para chegar ao seu destino. Que distância o sinal percorrel ?
 
-#velocidade da luz: 
+#velocidade da luz: c = 3*10**8 m/s
+
+# v = 2*10**8 m/s
